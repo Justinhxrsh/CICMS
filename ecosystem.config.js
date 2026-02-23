3139579457b3dfb8cedc8ceb43c623886df20a39
Deployer <deploy@example.com>
2026-02-23 22:15:43 +0530

    chore: prepare for deployment on Railway/Render/Heroku

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'delaford-server',
-            script: 'build/Delaford.js',
+            script: 'server.js',
             instances: 1,
             autorestart: true,
             watch: false,
